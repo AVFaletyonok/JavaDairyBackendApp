1c84e4472ae8359e4032c6f0e672e97a49da3264
Alexander <AVFaletyonok@yandex.ru>
2024-08-11 15:23:41 +0300

    develop Add getTaskByID(), getTaskAll(), html page tamplate

--- src/main/java/com/example/dairy/controller/TaskController.java
+++ src/main/java/com/example/dairy/controller/TaskController.java
@@ -3,9 +3,9 @@ package com.example.dairy.controller;
 import com.example.dairy.model.Task;
 import com.example.dairy.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class TaskController {
@@ -17,4 +17,20 @@ public class TaskController {
     public Task create(@RequestBody Task task) {
         return taskRepository.save(task);
     }
+
+    @GetMapping("/tasks/{id}")
+    public Task getTaskByID(@PathVariable(name = "id") long id) {
+        if (taskRepository.findById(id).isPresent())
+            return taskRepository.findById(id).get();
+        else
+            return null;
+    }
+
+    @GetMapping("/tasks")
+    public Iterable<Task> getTaskAll() {
+        return taskRepository.findAll();
+    }
+//    public String sayHello() {
+//        return "Hello!";
+//    }
 }
